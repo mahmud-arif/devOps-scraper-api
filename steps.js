720b16a079c6752bc1d395fd4114f6db029105b3
Taher <entrptaher@gmail.com>
2020-01-12 16:02:41 +0600

    init

---
+++ steps.js
@@ -0,0 +1,11 @@
+// queue
+// process
+// scraper
+// api
+// frontend
+// prisma + mongo
+
+// list all scrapped url, and content from database
+// if url doesn't exist, then grab it from the web, and add the data into database
+// use redis queue to schedule the scraper
+// data should be persisted over restart/be stored somewhere else
