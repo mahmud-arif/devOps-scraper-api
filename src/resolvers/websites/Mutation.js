63167ff9d367b188250a89876983b9fcd81b6bce
mahmud <mahmudarif1175@gmail.com>
2020-01-14 18:48:42 +0600

    feat: add bull queue

--- src/resolvers/websites/Mutation.js
+++ src/resolvers/websites/Mutation.js
@@ -1,13 +1,23 @@
 const getTitle = require('./utils/puppetter');
+const  Queue = require('bull');
+let scpQueue = new Queue('scrap queue');
 
+scpQueue.process(async(job, done)=>{
+    const title = await getTitle(job.data.url);  
+    done(null, {title: title})
+})
 
 async function createWebsite(parent, args, context, info){
     const {url} = args;
-    const title = await getTitle(url);  
-    return await context.prisma.createWebsite({
-        url,
-        title
-    })
+    scpQueue.add({url: url,})
+    scpQueue.on('completed', async(job)=>{
+        let title = job.title; 
+        await context.prisma.createWebsite({
+            url,
+            title
+        })
+    }) 
+    scpQueue.clean(100);
 }
 
 
